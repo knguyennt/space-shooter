a42f39a5d5302739b1d06b99f970ac5c35c4c235
knguyennt <khoanguyennt2010@gmail.com>
2025-07-03 00:35:10 +0700

    feat: add movement for player

---
+++ scenes/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var speed: int = 500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100,500) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if Input.is_action_pressed("right yeah"):
+		#position += Vector2(1,0) * 20 * delta
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += direction * speed * delta
