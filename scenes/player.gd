f5895233ca4bfd95be63c0fcf52f183b57c83b96
knguyennt <khoanguyennt2010@gmail.com>
2025-07-03 22:23:53 +0700

    feat: add collision

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed: int = 500
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +11,5 @@ func _process(delta: float) -> void:
 	#if Input.is_action_pressed("right yeah"):
 		#position += Vector2(1,0) * 20 * delta
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
